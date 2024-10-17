ba5e1807bbb4584e50b4d00e21ebe5ed8f854851
gigamicro <gammamicro@gmail.com>
2024-10-17 01:29:10 -0400

    tighten/expand it

--- boomerang.lua
+++ boomerang.lua
@@ -1,18 +1,42 @@
 local opts = {enabled=true}
--- TODO?: add mode for 1x forward, 1x reverse, then on to next file
 require'mp.options'.read_options(opts)
-local inprogress = false
+local lastloop
 mp.register_event('seek',function(ev)
 	if not opts.enabled then return end
-	if not mp.get_property_native('loop-file') then return end
+	local looptype =
+	mp.get_property_native('remaining-file-loops') ~= 0
+	 or 
+	mp.get_property_native('remaining-ab-loops') ~= 0
+	 and mp.get_property_native('ab-loop-a','no')~='no'
+	 and mp.get_property_native('ab-loop-b','no')~='no'
+
+	looptype, lastloop = looptype or lastloop, looptype
+	if not looptype then return end
+
+	local a = mp.get_property_native('ab-loop-a')
+	local b = mp.get_property_native('ab-loop-b')
+	if b < a then a,b=b,a end
 	local playdir = mp.get_property_native('play-direction')
-	local dur = mp.get_property_native('duration')
 	local pos = mp.get_property_native('time-pos/full')
-	if (playdir=='forward' and 0 or dur)==pos then -- just seeked to start of playback
-		if inprogress then inprogress=false return end
-		inprogress=true
-		mp.set_property_native('play-direction',playdir=='forward' and 'backward' or 'forward')
-		if playdir=='forward' then mp.commandv('seek',dur-0.0625) inprogress=false end -- when reversing, reseek
+	if a==pos and playdir=='forward' then
+		mp.set_property_native('play-direction','backward')
+		mp.commandv('seek',b-1/64,'absolute')
+		return
+	end
+	if b==pos and playdir=='backward' then
+		mp.set_property_native('play-direction','forward')
+		mp.commandv('seek',a+1/64,'absolute')
+		return
+	end
+	local d = mp.get_property_native('duration')
+	if 0==pos and playdir=='forward' then
+		mp.set_property_native('play-direction','backward')
+		mp.commandv('seek',d-1/12,'absolute')
+		return
+	end
+	if d==pos and playdir=='backward' then
+		mp.set_property_native('play-direction','forward')
+		mp.commandv('seek',0+1/64,'absolute')
 		return
 	end
 end)
